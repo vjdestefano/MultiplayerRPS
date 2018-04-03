62a45a2e726fb470df3384c01fe720a0c95185ec
Vinny DeStefano <vjdestefano500@gmail.com>
2018-04-03 16:38:03 -0400

    this is as far as i got

--- multiRPS.js
+++ multiRPS.js
@@ -15,14 +15,81 @@ var userChoice = "";
 var userScore = "";
 var roundScore = "";
 var chatMessage = "";
+var dateAdded = "";
+var inGame = false;
+
+function writeUserData(userId) {
+  firebase.database().ref('users/' + userId).set({
+    userChoice: userChoice,
+    chatMessage: chatMessage,
+  });
+}
 
 
-database.ref().push({
+window.onload = function() {
 
-  userName: userName,
-  userChoice: userChoice,
-  userScore: userScore,
-  roundScore: roundScore,
-  chatMessage: chatMessage,
+  userName = Math.floor(Math.random() * 1000);
+  if(userName === database.ref('users/' + userName).equalTo(userName)){
+    console.log("there is a name already here");
+  } else {
+    writeUserData(userName);
+  }
 
+}
+
+
+//dateAdded: firebase.database.ServerValue.TIMESTAMP
+
+$(".btn-choice").on("click", function() {
+  var btnChoice = $(this).text();
+
+  //userChoice = btnChoice;
+  database.ref('users/' + userName).update({
+    userChoice: btnChoice
+  });
+
+  console.log(btnChoice);
 });
+//make sure you are specific with what you want the "this" id to call
+
+$("#submitMessage").on("click", function() {
+  var message = $("#messageCenter").val().trim();
+  $("#messageCenter").val("");
+
+ // $("#chatLog").append(message);
+ database.ref('users/' + userName).update({
+  chatMessage: message,
+});
+  
+});
+
+$("#messageCenter").on("keyup", function(event) {
+  //event.preventDefault();
+  if (event.key === "Enter") {
+    var messageEnter = $("#messageCenter").val().trim();
+    
+    $("#messageCenter").val("");
+
+    //$("#chatLog").append(messageEnter);
+
+    database.ref('users/' + userName).update({
+      chatMessage: messageEnter,
+    });
+
+  }
+});
+
+$(".btn-Q").on("click", function(event){
+  inGame = true;
+  database.ref('users/' + userName).update({
+    QueueStatus: inGame,
+  });
+  database.ref('users/' + userName).once('value').then(function(snapshot) {
+    var queueVal = (snapshot.val().QueueStatus);
+    console.log(queueVal);
+    });
+    
+  });
+  
+  
+
